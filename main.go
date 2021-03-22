1e52fdb6bbecb50479f0efaa94e56e5a30ecf434
Katherine Gott <katherinegott@Katherines-MacBook-Pro.local>
2021-03-22 13:55:47 -0500

    using mutex

--- main.go
+++ main.go
@@ -12,40 +12,45 @@ var rnd = rand.New(rand.NewSource(time.Now().UnixNano()))
 
 func main() {
 	wg := &sync.WaitGroup{}
+	m := &sync.RWMutex{}
 	for i := 0; i < 10; i++ {
 		id := rnd.Intn(10) + 1
 		wg.Add(2)
-		go func(id int, wg *sync.WaitGroup) {
-			if b, ok := queryCache(id); ok {
+		go func(id int, wg *sync.WaitGroup, m *sync.RWMutex) {
+			if b, ok := queryCache(id, m); ok {
 				fmt.Println("from cache")
 				fmt.Println(b)
 
 			}
 			wg.Done()
-		}(id, wg)
-		go func(id int, wg *sync.WaitGroup) {
-			if b, ok := queryDatabase(id); ok {
+		}(id, wg, m)
+		go func(id int, wg *sync.WaitGroup, m *sync.RWMutex) {
+			if b, ok := queryDatabase(id, m); ok {
 				fmt.Println("from database")
 				fmt.Println(b)
 
 			}
 			wg.Done()
-		}(id, wg)
+		}(id, wg, m)
 		time.Sleep(150 * time.Millisecond)
 	}
 	wg.Wait()
 }
 
-func queryCache(id int) (Book, bool) {
+func queryCache(id int, m *sync.RWMutex) (Book, bool) {
+	m.RLock()
 	b, ok := cache[id]
+	m.RUnlock()
 	return b, ok
 }
 
-func queryDatabase(id int) (Book, bool) {
+func queryDatabase(id int, m *sync.RWMutex) (Book, bool) {
 	time.Sleep(100 * time.Millisecond)
 	for _, b := range books {
 		if b.ID == id {
-			// cache[id] = b
+			m.Lock()
+			cache[id] = b
+			m.Unlock()
 			return b, true
 		}
 	}
